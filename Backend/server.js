4e006e8eff14b7e77f7318bac0be0481b47d1191
sayeedkhannabil <sayeedkhannabil@gmail.com>
2022-02-26 18:11:57 -0600

    middleware created for userAPI. #49

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 5001;
 // database connection
 connectDB();
 
+// define routes and API
+app.use(express.json({ extended: false}));
+app.use("/api/users", require("./routes/userApi"));
+
 app.get("/", (req, res) => {
     res.send("App up");
 });
