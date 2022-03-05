6aa02843e7cef9800a7cd171680fbcd9dd32f3ae
Hasin Ishrak <32551736+ishrakHP@users.noreply.github.com>
2022-03-05 14:59:28 -0600

    Added post to authApi #43

--- Backend/routes/authApi.js
+++ Backend/routes/authApi.js
@@ -6,6 +6,14 @@ const User = require("../models/User");
 
 const auth = require("../middleware/authorization");
 
+const jwt = require("jsonwebtoken");
+
+const config = require("../config/keys");
+
+const bcrypt = require("bcryptjs");
+
+const {check, validationResult } = require("express-validator") ;
+
 rounter.get("/", auth, async (req, res)=> {
     try {
         const user = await User.findById(req.user.id).select("-password") ;
@@ -15,5 +23,48 @@ rounter.get("/", auth, async (req, res)=> {
         console.log(error.message);
     }    
 });
+
+rounter.post("/", [
+                    check("email", "email is required.").isEmail(),
+                    check("password", "Password is required.").exists()],
+                    async (req, res)=> {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    try {
+        const { email, password } = req.body ;
+        let user = await User.findOne({email: email });
+        if (!user) {
+            return res.status(400).json({errors: [{msg: "Invalied email or password!"}] });
+        }
+        const match = await bcrypt.compare(password, user.password);
+        
+
+        if(!match) {
+            return res.status(400).json({errors: [{msg: "Invalied email or password!"}] });
+        }
+
+        const payload = {
+            user:{
+                id: user.id,
+            },
+        };
+
+        jwt.sign(payload, config.jwtSecret, {expiresIn: 3600*24 },
+        (err, token) => {
+            if(err) throw err;
+            res.json({token});
+        }
+        ) ;
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server error.");
+    }
+
+    
+} );
 
 module.exports = rounter ;
