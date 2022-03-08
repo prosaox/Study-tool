d0fa040375a8591c8c8463c43f3f98225a113c1f
sayeedkhannabil <sayeedkhannabil@gmail.com>
2022-03-08 00:56:40 -0600

    created setAuthToken #43

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,20 +1,38 @@
-import {SET_CURRENT_USER} from "../actions/types"
+import {SET_CURRENT_USER, SUCCESSFULL_REGISTER, ERRORS} from "../actions/types"
 import {isEmpty} from 'lodash';
 
 const initialState = {
-    isAuthenticated : false,
+    isAuthenticated: false,
+    token: localStorage.getItem("token"),
     user: {}
 };
 
 export default function (state = initialState, action) {
+    const {payload} = action
     switch(action.type) {
         case SET_CURRENT_USER:
             return{
                 ...state,
                 isAuthenticated: !isEmpty(action.payload),
-                user: action.payload
+                user: payload
             }
+        case SUCCESSFULL_REGISTER:
+            localStorage.setItem("token", payload.token)
+            return{
+                ...state,
+                ...payload,
+                isAuthenticated: true,
+            }
+
+        case ERRORS:
+            localStorage.removeItem("token")
             
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false
+            }
+
         default:
             return state
     }
