e82f1b3913ad7d93e62e4804dfc4e8da2a500d3e
sayeedkhannabil <sayeedkhannabil@gmail.com>
2022-03-08 00:00:07 -0600

    auth action succesfull register setup #43

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,8 +1,32 @@
-import {SET_CURRENT_USER} from "./types"
+import axios from "axios";
+import {SET_CURRENT_USER, SUCCESSFULL_REGISTER, FAILURE_REGISTER, ERRORS} from "./types"
 
 export const setCurrentUser = (user) => {
     return {
         type: SET_CURRENT_USER,
         payload: user,
     };
+};
+
+export const register = (userData) => dispatch => {
+    const config = {
+        headers: {
+            "Content-type": "application/json"
+        },
+    };
+ 
+    try {
+        const res = axios.post("localhost:3001/api/users", userData, config)
+        dispatch({
+            type: SUCCESSFULL_REGISTER,
+            payload: res.data
+        });
+        
+    } catch(err) {
+        const error = err.response.data.errors;
+        dispatch({
+            type: ERRORS,
+            payload: error
+        });
+    }
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,6 +1,8 @@
-const ERRORS = "ERRORS";
-const SET_CURRENT_USER = "SET_CURRENT_USER";
-const GET_COURSE = "GET_COURSE";
-const Courses = "COURSES";
+export const ERRORS = "ERRORS";
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const GET_COURSE = "GET_COURSE";
+export const Courses = "COURSES";
+export const SUCCESSFULL_REGISTER = "SUCCESSFULL_REGISTER";
+export const FAILURE_REGISTER = "FAILURE_REGISTER";
 
 
