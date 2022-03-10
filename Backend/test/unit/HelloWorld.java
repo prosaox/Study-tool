4d939571502faf4f3385572f3a7cb0c61da54cf3
Hiep Nguyen <nguyen62@myumanitoba.ca>
2022-03-10 04:23:26 -0600

    new file:   Backend/test/unit/HelloWorld.java

---
+++ Backend/test/unit/HelloWorld.java
@@ -0,0 +1,10 @@
+package Backend.test.unit;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
