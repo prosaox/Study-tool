f742441bca22aa333f15f4232da13618f81caf52
Thomas Livingstone <thomas.livingstone.1006@gmail.com>
2022-03-11 15:43:33 -0600

    show courses on page for #42

--- client/src/components/courses.js
+++ client/src/components/courses.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './navbar'
 
-const courses = () => {
-  return (
-      <div>
-          <h1>StudyBuddy</h1>
-          <Navbar />
-          <h2>courses</h2>
-      </div>
-  )
+const Courses = () => {
+    const [courses, setCourses] = useState(null);
+
+    useEffect(() => {
+        const getCourses = async () => {
+            try {
+                const res = await fetch("http://localhost:5001/api/courses/", {
+                    method: "GET",
+
+                    headers: {
+
+                    },
+                })
+                    .then(res => res.json());
+                setCourses(res);
+            } catch (err) {
+
+            }
+        };
+        getCourses();
+    }, []);
+
+    if (courses == null) {
+        return <p> loading </p>;
+    } else {
+        return (
+            <div>
+                <h1>StudyBuddy</h1>
+                <Navbar />
+                <div className='container-fluid'>
+                    <h2>Courses</h2>
+                    {courses.map(c => <p key={c._id}>{c.name + ": " + c.description}</p>)}
+                </div>
+            </div>
+        )
+    }
 }
 
-export default courses
+export default Courses
