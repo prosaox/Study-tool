e727b93c6d1acb4d79b8d609983481845ca3f15e
Thomas Livingstone <thomas.livingstone.1006@gmail.com>
2022-03-11 15:15:29 -0600

    home displays users name for #29

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
 import Navbar from './navbar';
-// this is just a test homepage for initial setup of the react.js frontend
 
-const home = () => {
-  return (
-      <div>
-          <h1> StudyBuddy </h1>
-        <Navbar />
-          <p> This is a webapge created with react.js.</p>
-    </div>
-  )
+const Home = () => {
+    const [user, setUser] = useState(null);
+    const [name, setName] = useState('');
+
+    useEffect(() => {
+        const getUser = async () => {
+            const token = localStorage.getItem("token");
+            try {
+                const res = await fetch("http://localhost:5001/api/auth/", {
+                    method: "GET",
+
+                    headers: {
+                        "x-auth-token": token
+                    },
+                })
+                    .then(res => res.json());
+                setUser(res);
+                setName(res.name);
+            } catch (err) {
+
+            }
+        };
+        getUser();
+    },[]);
+
+    console.log("right before check : " + user);
+
+    if (user === null) {
+        return <p> loading </p>;
+    } else {
+        return (
+            <div>
+                <h1> StudyBuddy </h1>
+                <Navbar />
+                <p> Welcome, {name}!</p>
+            </div>
+        )
+    }
 }
 
-export default home;
+export default Home;
