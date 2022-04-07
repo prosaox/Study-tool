b549f17e714c9b67232bdd88fc259a246a62ffcb
sayeedkhannabil <sayeedkhannabil@gmail.com>
2022-04-07 17:00:52 -0500

    flashcard schema created. #72

--- Server/app.js
+++ Server/app.js
@@ -9,7 +9,9 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/userApi"));
 app.use("/api/auth", require("./routes/authApi"));
 app.use("/api/courses", require("./routes/courseApi"));
-app.use("/api/tasks", require("./routes/taskApi"))
+app.use("/api/tasks", require("./routes/taskApi"));
+app.use("api/flashcards", require("./routes/flashcardApi"));
+
 app.get("/", (req, res) => {
     res.send("App up");
 });
